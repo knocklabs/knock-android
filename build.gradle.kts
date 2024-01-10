606462258f5188f311dd9a62c038f89e68b8f14b
Matt Gardner <mattgardner@Knock-MBP.local>
2024-01-09 21:04:44 -0700

    Adding needed gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.android.library") version "8.0.0" apply false
+}
