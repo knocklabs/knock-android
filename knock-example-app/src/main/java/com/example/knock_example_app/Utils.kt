fabae83d6d91cfd080c7c8a60412e5b1ac0f2ac5
Matt Gardner <mgardner@knock.app>
2024-03-07 18:38:12 -0700

    clearing out local variables

--- knock-example-app/src/main/java/com/example/knock_example_app/Utils.kt
+++ knock-example-app/src/main/java/com/example/knock_example_app/Utils.kt
@@ -1,9 +1,9 @@
 package com.example.knock_example_app
 
 object Utils {
-    val publishableKey = "pk_test_d0dB4Brd4GpuGd4_9JkMoUuDfPbQ-X-om2gKCROMQ9M"
-    val userId = "dnedry@ingen.net"
-    val inAppChannelId = "3b97fa62-4d20-40ef-93c8-850b4e7e9734"
-    val pushChannelId = "9c02a9aa-7664-4606-9ba8-cefab3a40ac2"
+    val publishableKey = ""
+    val userId = ""
+    val inAppChannelId = ""
+    val pushChannelId = ""
     val hostname = "https://api.knock-dev.app"
 }
